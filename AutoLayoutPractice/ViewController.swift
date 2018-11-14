c6a71f8b88edbfb052acfab5a9a1c11906f143d8
marte14 <kattymarte14@gmail.com>
2018-11-14 14:37:10 -0500

    first commit

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by katty y marte on 11/14/18.
+//  Copyright © 2018 katty y marte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
